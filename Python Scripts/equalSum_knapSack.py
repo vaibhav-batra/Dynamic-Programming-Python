863fa008a07866dfd62b5550446c4c0630286a59
vaibhav-batra <vaibhav2793@gmail.com>
2021-02-25 10:56:07 +0530

    Adding equal sum knapsack

---
+++ Python Scripts/equalSum_knapSack.py
@@ -0,0 +1,40 @@
+import time
+
+# SubSet Sum Knapsack
+
+
+def subsetSum(set, n):
+
+    sum = 0
+
+    # For two partitions of equal value, total value must be even
+    # If Even, we only want to find a subset of of value sum/2
+    for i in set:
+        sum+= i
+
+    if sum%2!=0:
+        return False
+
+    K = [[-1 for i in range(int(sum/2)+1)] for i in range (n)]
+
+    for i in range(1,n):
+        # i is iterating over the indexes of elements
+        for j in range(1,(int(sum)/2)+1):
+            # j is iterating over values of S
+            if set[i-1] > j:
+            # Don't include
+                K[i][j] = K[i-1][j]
+            else:
+                # Check both case where include or not and if either of them is True
+                K[i][j] = K[i-1][j] or K[i-1][j-set[i-1]]
+    return K[n][S]
+
+if __name__ == "__main__":
+
+    wt = [1,5,5,11]
+    n = len(wt)
+    start = time.time()
+    a = subsetSum(wt, n, S)
+    end = time.time()
+    # print("Time elapsed with Memoization:{}".format(end-start))
+    print(a)
